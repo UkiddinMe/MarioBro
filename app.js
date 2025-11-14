2dc89bd441ca24ae33f5dbe6a68e8ca7eea1ce7c
Tommaso Castagnola <tommaso.castagnola@pomiager.com>
2025-11-14 16:53:29 +0100

    interazioni con monete blocchi e nemici e big Mario

--- app.js
+++ app.js
@@ -262,6 +262,10 @@ function gameLoop() {
     if (!gameState.gameRunning) return
 
     update()
+
+    // requestAnimationFrame è una funzione javascript che dice al browser che vuoi fare un'animazione.
+    // Chiede al browser di eseguire una data funzione di callback PRIMA del prossimo repaint.
+    // E' one-shot, quindi va richiamata
     requestAnimationFrame(gameLoop)
 }
 
@@ -297,12 +301,104 @@ function update()
     player.grounded = false;
     for (let platform of gameObjects.platforms) {
         if (checkCollision(player, platform)) {
-            if (player.velocityY > 0) {
+            if (player.velocityY > 0) { //Falling
                 player.y = platform.y - player.height
                 player.velocityY = 0
                 player.grounded = true
             }
         }
+    }
+
+    // Pipe collision
+    for (let pipe of gameObjects.pipes) {
+        if (checkCollision(player, pipe)) {
+            if (player.velocityY > 0) { // Fallin down onto Pipe
+                player.y = pipe.y - player.height
+                player.velocityY = 0
+                player.grounded = true
+            }
+        }
+    }
+
+    // Enemy movement and collision
+    for (let enemy of gameObjects.enemies) {
+        if (!enemy.alive) continue
+
+        enemy.x += enemy.speed * enemy.direction;
+
+        let onPlatform = false
+        // Reverse direction at platform edges or boundaries
+        for (let platform of gameObjects. platforms) {
+            if (enemy.x + enemy.width > platform.x &&
+                enemy.x < platform.x + platform.width && 
+                enemy.y + enemy.height >= platform.y -5 &&
+                enemy.y + enemy.height <= platform.y +5
+            ) {
+                onPlatform = true
+                break
+            }
+        }
+
+        if (!onPlatform || enemy.x <= 0 || enemy.x >= 800) {
+            enemy.direction *= -1 //inverti il valore da positivo o negativo e vice versa
+        }
+
+        updateElementPosition(enemy.element, enemy.x, enemy.y)
+        
+        // Check player-enemy collision
+        if (checkCollision(player, enemy)) {
+            if (player.velocityY > 0 && player.y < enemy.y) { // je stai saltando sopra
+                enemy.alive = false
+                enemy.element.remove();
+                player.velocityY = JUMP_FORCE * 0.7
+                gameState.score += 100
+            } else {
+                // hit by enemy
+                if (player.big) {
+                    player.big = false
+                    player.bigTimer = 0
+                    player.element.classList.remove('big')
+                    player.width = 20
+                    player.height = 20
+                } else {
+                    //loseLife()
+                }
+
+            }
+        }
+
+
+        // Coin Collection
+        for (let coin of gameObjects.coins) {
+            if (!coin.collected && checkCollision(player, coin)) {
+                coin.collected = true
+                coin.element.remove()
+                gameState.score += 50
+            }
+        }
+
+        // Surprise blocks
+        for (let block of gameObjects.surpriseBlocks) {
+            if (!block.hit && checkCollision(player, block) && player.velocityY < 0) {
+                block.hit = true
+                block.element.classList.add('hit')
+
+                if (block.type === 'mushroom') {
+                    player.big = true
+                    player.bigTimer = 600
+                    player.element.classList.add('big')
+                    player.width = 30
+                    player.height = 30
+                    gameState.score += 100
+                } else if (block.type === 'coin') {
+                    gameState.score += 50
+                }
+            }
+        }
+
+        // Pipe interaction to next level
+
+
     }
 
     updateElementPosition(player.element, player.x, player.y)
